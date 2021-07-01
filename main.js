c0de9e922fd7d3a74b4eac7615feac4f577327b1
goroy2038 <rust2038@yandex.ru>
2021-07-02 01:54:34 +0400

    v1

---
+++ main.js
@@ -0,0 +1,19 @@
+let input1Value = document.getElementById("input1Value");
+let input2Value = document.getElementById("input2Value");
+let result = document.getElementById("result");
+let clearButton = document.getElementById("clearButton");
+let addButton = document.getElementById("addButton");
+
+
+addButton.onclick = function (){
+    let a = Number(input1Value.value);
+    let b = Number(input2Value.value);
+    result.value = a + b;
+}
+
+clearButton.onclick = function(){
+    input1Value.value = "";
+    input2Value.value = "";
+    result.value = "";
+    input1Value.focus();
+}
